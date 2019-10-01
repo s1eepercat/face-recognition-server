72fb1aebbb056f247c013c28d4d455fc64853ace
Aleksandr Jevlentjev <jevlentjev@gmail.com>
2019-10-01 19:09:05 +0300

    test2

---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '1311b8f1eae5417080d3b609d05fef9f'
+});
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('Unable to work with api'))
+}
+
+const imageHandler = (req, res, db) => {
+    const { id } = req.body;
+
+    db('users')
+        .where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('Unable to get entries'))
+}
+
+module.exports = {
+    imageHandler: imageHandler,
+    handleApiCall: handleApiCall
+}
